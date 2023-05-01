f198fad61fc32b9a013e658a6fc3e7697fba6102
SpaceCatleta <spacecatleta@gmail.com>
2023-05-01 11:49:01 +0300

    добавлена и заполнена таблица игр

--- gamesShop/shop/admin.py
+++ gamesShop/shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import GameDetail
 
-# Register your models here.
+
+class GameDetailAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Main info', {'fields': ['label', 'cover', 'release_date']}),
+        ('More information', {'fields': ['developer', 'publisher', 'game_version', 'description']}),
+    ]
+
+
+admin.site.register(GameDetail, GameDetailAdmin)
--- gamesShop/shop/models.py
+++ gamesShop/shop/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class GameDetail(models.Model):
+    label = models.CharField(max_length=100)
+    cover = models.ImageField('Обложка игры', upload_to='game_covers')
+    release_date = models.DateField('дата выхода')
+
+    developer = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    game_version = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+
+
+
+    def __str__(self):
+        return f'{self.label}, {self.release_date}'
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
