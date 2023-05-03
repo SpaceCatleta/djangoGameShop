048e0d912575bf6a583dd1222fb4b36842ff5796
SpaceCatleta <spacecatleta@gmail.com>
2023-05-03 19:23:19 +0300

    добавлены стилии

--- gamesShop/shop/models.py
+++ gamesShop/shop/models.py
@@ -16,7 +16,6 @@ class GameDetail(models.Model):
     description = models.CharField(max_length=2000)
 
 
-
     def __str__(self):
         return f'{self.label}, {self.release_date}'
 
--- gamesShop/shop/views.py
+++ gamesShop/shop/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    print('CATALOG')
+    counter = int(request.session.get('num_visits', 0)) + 1
+    request.session['num_visits'] = str(counter)
+    request.session.modified = True
+
+    context =  {'num_visits': f'{counter}',
+                'admin': True}
+    return render(request, "shop/index.html", {'context': context})
+
+
+def library(request):
+    print('LIBRARY')
+    context = {}
+    return render(request, "shop/library.html", {'context': context})
